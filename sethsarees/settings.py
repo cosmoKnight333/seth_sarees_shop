5ce8445b81992ec0fd84e86e0abab281b68ce435
cosmoKnight333 <66382668+cosmoKnight333@users.noreply.github.com>
2023-01-23 19:20:25 +0530

    almost completed

--- sethsarees/settings.py
+++ sethsarees/settings.py
@@ -16,7 +16,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sethsarees@gmail.com'
-EMAIL_HOST_PASSWORD = 'ersberqhndflvdxi'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
